45204ccba54810396469ea6fcf03c10db405f7db
Jan Lüthje <37576268+LLoydcc@users.noreply.github.com>
2023-02-21 18:57:12 +0100

    adjust clocking.js

--- components/clockin.js
+++ components/clockin.js
@@ -3,35 +3,38 @@ import Paper from "@mui/material/Paper";
 import Stack from "@mui/system/Stack";
 import Button from "@mui/material/Button";
 import RestartAltIcon from "@mui/icons-material/RestartAlt";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
+import Moment from "moment";
 
-/*
-clockin works a bit different than a normal timer (as timer.js). 
-
-In the default usecase the worktime gets billed in 15min. sequences (can be adjusted if needed later). 
-So there's no need to display the time every second and minute. Also we will use a rounded time for the clockin - 
-meaning if we clockin @ 13:17 the worktime starts at 13:15. If we clock in @ 13:25, the worktime starts at 13:30.
+/** 
+ * clockin.js is a variation of the standard timer (timer.js).
+ * In the default usecase the worktime gets billed in a sequence of 15 minutes (can be adjusted if needed). 
+ * This results in a rounded starting time after the clockin. Meaning if the clockin takes place at 13:23 the
+ * shift will start at 13:30. If the clockin is made at 13:17 the shift is already running for 2 minutes.
 */
 
 export const Clockin = React.forwardRef((props, ref) => {
   const [isClockedIn, setIsClockedIn] = React.useState(false);
+  const [clockedInAt, setClockedInAt] = React.useState(new Date());
   const [workTimeInSeconds, setWorkTimeInSeconds] = React.useState(0);
-  const [clockedInAt, setClockedInAt] = React.useState(null);
-  const [shiftIsRunning, setShiftIsRunning] = React.useState(false);
-  const [hours, setHours] = React.useState(0);
-  const [openDialog, setOpenDialog] = React.useState(false);
-  const [reset, setReset] = React.useState(false);
+
+  React.useEffect(() => {
+    if (isClockedIn) {
+      initClockedInRoundedDate();
+    }
+  }, [isClockedIn]);
 
   React.useEffect(() => {
     let interval = null;
     if (isClockedIn) {
-      initClockedInRoundedDate();
       interval = setInterval(() => {
-        setWorkTimeInSeconds((workTimeInSeconds) => workTimeInSeconds + 1);
+        const currentTime = new Date();
+        const seconds = calculateSecondsBetweenTwoDates(
+          currentTime,
+          clockedInAt
+        );
+        if (seconds > 0) {
+          setWorkTimeInSeconds(seconds);
+        }
       }, 1000);
     } else {
       clearInterval(interval);
@@ -39,52 +42,14 @@ export const Clockin = React.forwardRef((props, ref) => {
     return () => {
       clearInterval(interval);
     };
-  }, [isClockedIn]);
+  }, [clockedInAt]);
 
   React.useEffect(() => {
-    if (!shiftIsRunning) {
-      const current = new Date();
-      if (clockedInAt != null && current >= clockedInAt) {
-        setShiftIsRunning(true);
-      }
-    } else {
-      const count = Math.floor(workTimeInSeconds / 3600);
-      if (hours < count) {
-        setHours(count);
-      }
-    }
+    console.log(workTimeInSeconds);
   }, [workTimeInSeconds]);
 
-  React.useEffect(() => {
-    props.onHourChange(hours);
-  }, [hours]);
-
-  React.useEffect(() => {
-    if (shiftIsRunning) {
-      const seconds = calculateSecondsBetweenTwoDates(clockedInAt, new Date());
-      setWorkTimeInSeconds(seconds);
-    }
-  }, [shiftIsRunning]);
-
   const calculateSecondsBetweenTwoDates = (first, second) => {
-    return Math.round(Math.abs(first.getTime() - second.getTime()) / 1000);
-  };
-
-  const handleClockInClockOut = () => {
-    setIsClockedIn(!isClockedIn);
-  };
-
-  const handleReset = () => {
-    setOpenDialog(true);
-  };
-
-  const handleResetAgree = () => {
-    setWorkTimeInSeconds(0);
-    setIsClockedIn(false);
-    setClockedInAt(null);
-    setShiftIsRunning(false);
-    setHours(0);
-    setOpenDialog(false);
+    return Math.round((first.getTime() - second.getTime()) / 1000);
   };
 
   const initClockedInRoundedDate = () => {
@@ -95,48 +60,27 @@ export const Clockin = React.forwardRef((props, ref) => {
     setClockedInAt(roundedDate);
   };
 
+  const handleClockInClockOut = () => {
+    setIsClockedIn(!isClockedIn);
+  };
+
   const formatShiftStartDate = () => {
     return (
       <>
         <span style={{ color: "#1976d2" }}>START:</span>
         <span style={{ marginLeft: "3px" }}>
-          {("0" + clockedInAt.getHours()).slice(-2) +
-            ":" +
-            ("0" + clockedInAt.getMinutes()).slice(-2)}
+          {Moment(clockedInAt).format("HH:mm")}
         </span>
-        <span style={{ marginLeft: "4px", color: "#1976d2" }}>UHR</span>
+        <span style={{ marginLeft: "3px", color: "#1976d2" }}>UHR</span>
       </>
     );
   };
 
   const formatWorkTime = () => {
-    const getHours = `0${Math.floor(workTimeInSeconds / 3600)}`.slice(-2);
-    const minutes = `${Math.floor(workTimeInSeconds / 60)}`;
-    const getMinutes = `${minutes % 60}`;
-    const quarters = Math.floor(getMinutes / 15);
-
-    console.log('seconds', workTimeInSeconds);
-    console.log('minutes', getMinutes);
-    
-    let getQuarters = ``;
-    switch (quarters) {
-      case 0:
-        getQuarters = `00`;
-        break;
-      case 1:
-        getQuarters = `15`;
-        break;
-      case 2:
-        getQuarters = `30`;
-        break;
-      case 3:
-        getQuarters = `45`;
-        break;
-      default:
-        break;
-    }
-
-    return `${getHours} : ${getQuarters}`;
+    const seconds = ("0" + (workTimeInSeconds % 60)).slice(-2);
+    const minutes = ("0" + (Math.floor(workTimeInSeconds / 60) % 60)).slice(-2);
+    const hours = ("0" + Math.floor(workTimeInSeconds / 3600)).slice(-2);
+    return hours + " : " + minutes + " : " + seconds;
   };
 
   return (
@@ -145,28 +89,6 @@ export const Clockin = React.forwardRef((props, ref) => {
         padding: "10px",
       }}
     >
-      <Dialog
-        open={openDialog}
-        onClose={() => setOpenDialog(false)}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Zeit zurücksetzen?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Alle nicht gespeicherten Änderungen gehen verloren und können nicht
-            wiederhergestellt werden.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => setOpenDialog(false)}>Nein</Button>
-          <Button onClick={() => handleResetAgree()} autoFocus>
-            Ja
-          </Button>
-        </DialogActions>
-      </Dialog>
       <Stack
         direction="row"
         justifyContent="center"
@@ -177,7 +99,7 @@ export const Clockin = React.forwardRef((props, ref) => {
       >
         <span ref={ref}>{formatWorkTime()}</span>
       </Stack>
-      {clockedInAt !== null && (
+      {isClockedIn && (
         <Stack
           direction="row"
           justifyContent="center"
@@ -204,9 +126,6 @@ export const Clockin = React.forwardRef((props, ref) => {
           color={isClockedIn ? "error" : "success"}
         >
           {isClockedIn ? "Ausstempeln" : "Einstempeln"}
-        </Button>
-        <Button variant="outlined" onClick={handleReset}>
-          <RestartAltIcon></RestartAltIcon>
         </Button>
       </Stack>
     </Paper>
